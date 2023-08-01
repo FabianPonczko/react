c1e5267d58a54a5f874838673ebac6a57fadc581
FabianPonczko <fabianponczko@live.com.ar>
2023-08-01 17:42:15 -0300

    avances

--- src/Component/ItemDetailContainer/ItemDetailContainer.js
+++ src/Component/ItemDetailContainer/ItemDetailContainer.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import ItemDetail from "../ItemDetail/ItemDetail"
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { getProductId } from "../../services/firebase/firestore"
 import './ItemDetailContainer.css'
 
@@ -34,6 +34,10 @@ if(loading){
     return(
       <div className="item-detail">
             <div className="conteiner">
+                <Link to={`/category/${products.category}`}>
+                  <h5 className="volver">{`<- volver - producto ${products.category}`}</h5>
+                </Link>
+                
                 <h2 className="title">{products.title}</h2>
                 <ItemDetail  detailProduct= {products} />
             </div>
